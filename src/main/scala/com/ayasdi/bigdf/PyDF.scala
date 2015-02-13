c6606901a6df19418a8fa266f9795697edd35151
Mohit Jaggi <mohit@ayasdi.com>
2015-02-12 18:19:23 -0800

    add other comparators
    Former-commit-id: 7a08870f4226ead36575f39bda944602ef9f0b7c

--- src/main/scala/com/ayasdi/bigdf/PyDF.scala
+++ src/main/scala/com/ayasdi/bigdf/PyDF.scala
@@ -1,4 +1,4 @@
-/* Ayasdi Inc. Copyright 2014 - all rights reserved. */
+/* Ayasdi Inc. Copyright 2015 - all rights reserved. */
 /**
  * @author abhishek, ajith, mohit
  *         big dataframe on spark: wrappers for python access via py4j
@@ -8,61 +8,72 @@ package com.ayasdi.bigdf
 import org.apache.spark.SparkContext
 import scala.reflect.runtime.{universe => ru}
 import scala.reflect.{ClassTag, classTag}
+import Preamble._
 
 case class PyDF(df: DF) {
-    def columnNames = df.columnNames
-    def column(name: String) = PyColumn(df.column(name))
-    def list = df.list   
-    def describe = df.describe
-    
-    def where(predicate: PyPredicate) : PyDF = {
-      PyDF(df.where(predicate.p))
-    }
-    
-//    def where(columnName: String, operator: String, value: Double) : PyDF = {
-//       val filter = operator match {
-//         case "==" => df(columnName) == value
-//         case "!=" => df(columnName) != value
-////         case "<"  => df(columnName) < value
-////         case "<=" => df(columnName) <= value
-////         case ">"  => df(columnName) > value
-////         case ">=" => df(columnName) >= value
-//       }
-//       PyDF(df.where(filter))
-//    }
-}
+  def columnNames = df.columnNames
 
-case class PyColumn[+T: ru.TypeTag](col: Column[T]) {
-    def list(numRows: Int) = col.list(numRows)
-}
+  def column(name: String) = PyColumn(df.column(name))
 
-case class PyPredicate(p : Predicate) {
-    def And(that: PyPredicate) = {
-        PyPredicate(this.p && that.p)
-    }
-    
-    def Or(that: PyPredicate) = {
-        PyPredicate(this.p || that.p)
-    }
-    
-    def Not() = {
-        PyPredicate(!this.p)
-    }
-}
+  def list = df.list
 
-object PyPredicate {
-     def where[T](column: PyColumn[T], operator: String, value: Double) : PyPredicate = {
-       val filter = operator match {
-         case "==" => column.col == value
-         case "!=" => column.col != value
-       }
-       PyPredicate(filter)
-    }
+  def describe = df.describe
+
+  def where(predicate: PyPredicate): PyDF = {
+    PyDF(df(predicate.p))
+  }
 }
 
 object PyDF {
-  def buildDF(sc: SparkContext, name: String, separator: String, fasterGuess: Boolean): PyDF = {
-    val sep : Char = separator.charAt(0)
+  def fromCSV(sc: SparkContext, name: String, separator: String, fasterGuess: Boolean): PyDF = {
+    val sep: Char = separator.charAt(0)
     PyDF(DF(sc, name, sep, fasterGuess))
   }
 }
+
+
+case class PyColumn[+T: ru.TypeTag](col: Column[T]) {
+  def list(numRows: Int) = col.list(numRows)
+}
+
+case class PyPredicate(p: Predicate) {
+  def And(that: PyPredicate) = {
+    PyPredicate(this.p && that.p)
+  }
+
+  def Or(that: PyPredicate) = {
+    PyPredicate(this.p || that.p)
+  }
+
+  def Not() = {
+    PyPredicate(!this.p)
+  }
+}
+
+object PyPredicate {
+  def where[T](column: PyColumn[T], operator: String, value: Double): PyPredicate = {
+    val filter = operator match {
+      case "==" => column.col == value
+      case "!=" => column.col != value
+      case "<" => column.col < value
+      case "<=" => column.col <= value
+      case ">" => column.col > value
+      case ">=" => column.col >= value
+    }
+    PyPredicate(filter)
+  }
+
+  def where[T](column: PyColumn[T], operator: String, value: String): PyPredicate = {
+    val filter = operator match {
+      case "==" => column.col == value
+      case "!=" => column.col != value
+      case "<" => column.col < value
+      case "<=" => column.col <= value
+      case ">" => column.col > value
+      case ">=" => column.col >= value
+    }
+    PyPredicate(filter)
+  }
+}
+
+
