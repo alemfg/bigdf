0cf6da7c7d8ee5c287cfca1b237139b1bdd150f7
Mohit Jaggi <mohit@ayasdi.com>
2015-07-09 16:25:31 -0700

    map on column(s)
    Former-commit-id: c5c38414e0a9df2d0578be409b260ffe149e9c0f

--- src/main/scala/com/ayasdi/bigdf/RichColumnSeq.scala
+++ src/main/scala/com/ayasdi/bigdf/RichColumnSeq.scala
@@ -5,15 +5,17 @@
  */
 package com.ayasdi.bigdf
 
-import org.apache.spark.rdd.RDD
+import scala.reflect.runtime.{universe => ru}
 
-import scala.reflect.{ClassTag, classTag}
+import org.apache.spark.sql.functions._
 
 /**
  * Sequence of columns from a DF
  * @param cols  sequence of pairs. Each pair is a column name and the Column
  */
-case class RichColumnSeq(val cols: Seq[Column]) {
+case class RichColumnSeq(cols: Seq[Column]) {
+
+  lazy val df = cols.head.df.get
 
   def describe() {
     cols.foreach { col =>
@@ -22,11 +24,34 @@ case class RichColumnSeq(val cols: Seq[Column]) {
     }
   }
 
-  override def toString() = {
+  override def toString = {
     cols.map { x =>
       "\n" + x.name + ":\n" + x.toString
     }.reduce { (strLeft, strRight) =>
       strLeft + strRight
     }
   }
+
+  /**
+   * apply a given function to a column to generate a new column
+   * the new column does not belong to any DF automatically
+   */
+  def map[RT: ru.TypeTag, A1: ru.TypeTag](mapper: A1 => RT) = {
+    require(cols.length == 1)
+    val newCol = callUDF(mapper, SparkUtil.typeTagToSql(ru.typeOf[RT]), df.sdf(cols(0).name))
+    new Column(newCol)
+  }
+
+  /**
+   * apply a given function to 2 columns to generate a new column
+   * the new column does not belong to any DF automatically
+   */
+  def map[RT: ru.TypeTag, A1: ru.TypeTag, A2: ru.TypeTag](mapper: (A1, A2) => RT) = {
+    require(cols.length == 2)
+    val newCol = callUDF(mapper, SparkUtil.typeTagToSql(ru.typeOf[RT]),
+      df.sdf(cols(0).name),
+      df.sdf(cols(1).name))
+    new Column(newCol)
+  }
+
 }
