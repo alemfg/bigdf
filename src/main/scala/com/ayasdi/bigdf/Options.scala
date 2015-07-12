e822acd1ef5459032ec291606a7aabffb99e4c02
Mohit Jaggi <mohit@ayasdi.com>
2015-07-12 11:09:33 -0700

    caching option
    Former-commit-id: 7876e2697b72890fe1e8d53ca2e33981e8e67ff7

--- src/main/scala/com/ayasdi/bigdf/Options.scala
+++ src/main/scala/com/ayasdi/bigdf/Options.scala
@@ -5,20 +5,17 @@
  */
 package com.ayasdi.bigdf
 
-import org.apache.spark.storage.StorageLevel
-import com.databricks.spark.csv.{CSVParsingOpts, LineParsingOpts, StringParsingOpts, NumberParsingOpts}
+import com.databricks.spark.csv.{CSVParsingOpts, LineParsingOpts, NumberParsingOpts, StringParsingOpts}
 
 /**
- *
+ * Schema inference options
  * @param fastSamplingSize the number of samples to look at to guess the type of a field in a CSV file
  * @param fastSamplingEnable set to false to use a slower but possibly more accurate guesssing method
  */
-case class SchemaGuessingOpts(val fastSamplingSize: Int = 5,
-                              val fastSamplingEnable: Boolean = true)
+case class SchemaGuessingOpts(fastSamplingSize: Int = 5,
+                              fastSamplingEnable: Boolean = true)
 
-case class PerfTuningOpts(val storageLevel: StorageLevel = StorageLevel.MEMORY_ONLY_SER,
-                          val filterWithRowStrategy: Boolean = false)
-
+case class PerfTuningOpts(cache: Boolean = true)
 /**
  * bigdf configurable options with default values
  * @param numberParsingOpts options to control parsing of numbers
@@ -28,9 +25,9 @@ case class PerfTuningOpts(val storageLevel: StorageLevel = StorageLevel.MEMORY_O
  * @param schemaGuessingOpts schema guessing options
  * @param perfTuningOpts options to tune performance
  */
-case class Options(val numberParsingOpts: NumberParsingOpts = NumberParsingOpts(),
-                   val stringParsingOpts: StringParsingOpts = StringParsingOpts(),
-                   val lineParsingOpts: LineParsingOpts = LineParsingOpts(),
-                   val csvParsingOpts: CSVParsingOpts = CSVParsingOpts(),
-                   val schemaGuessingOpts: SchemaGuessingOpts = SchemaGuessingOpts(),
-                   val perfTuningOpts: PerfTuningOpts = PerfTuningOpts())
+case class Options(numberParsingOpts: NumberParsingOpts = NumberParsingOpts(),
+                   stringParsingOpts: StringParsingOpts = StringParsingOpts(),
+                   lineParsingOpts: LineParsingOpts = LineParsingOpts(),
+                   csvParsingOpts: CSVParsingOpts = CSVParsingOpts(),
+                   schemaGuessingOpts: SchemaGuessingOpts = SchemaGuessingOpts(),
+                   perfTuningOpts: PerfTuningOpts = PerfTuningOpts())
