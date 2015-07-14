f01d88d6eb0cf8c552e4e4c5176478559c69bf68
Mohit Jaggi <mohit@ayasdi.com>
2015-07-14 14:57:23 -0700

    expand sparse column of longs
    Former-commit-id: 130c610ce952402c96d26926f47d44e66f50db99

--- src/main/scala/com/ayasdi/bigdf/SparseColumnFunctions.scala
+++ src/main/scala/com/ayasdi/bigdf/SparseColumnFunctions.scala
@@ -10,17 +10,27 @@ import java.util.{HashSet => JHashSet}
 import scala.collection.JavaConversions.asScalaSet
 import scala.collection.mutable
 
+import scala.reflect.runtime.{universe => ru}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{FloatType, StringType, MapType}
+import org.apache.spark.sql.types.FloatType
 
 class SparseColumnFunctions(self: Column) {
 
   def expand(df: DF, keys: Set[String] = null, namePrefix: String = "expanded_"): Unit = {
-    require(self.colType == ColType.MapOfStringToFloat) //TODO: support can be added for others
+    require(self.colType == ColType.MapOfStringToFloat || self.colType == ColType.MapOfStringToLong)
 
     val ks = Option(keys) getOrElse {
-      self.mapOfStringToFloatRdd
-        .aggregate(AggDistinctKeys.zeroVal)(AggDistinctKeys.seqOp, AggDistinctKeys.combOp)
+      self.colType match {
+        case ColType.MapOfStringToFloat =>
+          val aggtor = AggDistinctKeys[Float]
+          self.mapOfStringToFloatRdd.aggregate(aggtor.zeroVal)(aggtor.seqOp, aggtor.combOp)
+
+        case ColType.MapOfStringToLong =>
+          val aggtor = AggDistinctKeys[Long]
+          self.mapOfStringToLongRdd.aggregate(aggtor.zeroVal)(aggtor.seqOp, aggtor.combOp)
+
+        case _ => throw new IllegalStateException("can't get here")
+      }
     }
 
     ks.foreach { k =>
@@ -33,10 +43,10 @@ class SparseColumnFunctions(self: Column) {
 
 }
 
-case object AggDistinctKeys {
+case class AggDistinctKeys[T: ru.TypeTag] {
   def zeroVal: mutable.Set[String] = new JHashSet[String]
 
-  def seqOp(a: mutable.Set[String], b: Map[String, Float]) = a ++= b.keySet
+  def seqOp(a: mutable.Set[String], b: Map[String, T]) = a ++= b.keySet
 
   def combOp(a: mutable.Set[String], b: mutable.Set[String]) = a ++= b
 }
